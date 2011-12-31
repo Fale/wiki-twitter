1b66b710d87d4b080f73a6b9037122ff5a325193
Fabio Alessandro Locati <flocati@grimp.eu>
2011-12-31 01:02:27 +0100

    Remove pointless stuff

--- classes/account.php
+++ classes/account.php
@@ -99,56 +99,5 @@ class Account
             die( "Invalid query: " . mysql_error() );
 //        Account::close( $db );
     }
-
-    function mysql_import($filename)
-    {
-        $return = false;
-        $sql_start = array('INSERT', 'UPDATE', 'DELETE', 'DROP', 'GRANT', 'REVOKE', 'CREATE', 'ALTER');
-    $sql_run_last = array('INSERT');
-
-    if (file_exists($filename)) {
-        $lines = file($filename);
-        $queries = array();
-        $query = '';
-
-        if (is_array($lines)) {
-            foreach ($lines as $line) {
-                $line = trim($line);
-
-                if(!preg_match("'^--'", $line)) {
-                    if (!trim($line)) {
-                        if ($query != '') {
-                            $first_word = trim(strtoupper(substr($query, 0, strpos($query, ' '))));
-                            if (in_array($first_word, $sql_start)) {
-                                $pos = strpos($query, '`')+1;
-                                $query = substr($query, 0, $pos) . $wpdb->prefix . substr($query, $pos);
-                            }
-
-                            $priority = 1;
-                            if (in_array($first_word, $sql_run_last)) {
-                                $priority = 10;
-                            } 
-
-                            $queries[$priority][] = $query;
-                            $query = '';
-                        }
-                    } else {
-                        $query .= $line;
-                    }
-                }
-            }
-
-            ksort($queries);
-
-            foreach ($queries as $priority=>$to_run) {
-                foreach ($to_run as $i=>$sql) {
-                    mysql_query($sql, $this->db);
-                }
-            }
-        }
-    }
-}
 } 
-
 ?>
-
