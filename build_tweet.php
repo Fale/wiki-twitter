72e19ca7944caafc79b8ad2197bb4a4284cba967
Fabio Alessandro Locati <flocati@grimp.eu>
2011-12-31 13:05:56 +0100

    create the first kind of class

--- build_tweet.php
+++ build_tweet.php
@@ -2,7 +2,30 @@
 ### CLASSES ###
 require_once( "classes/db.php" );
 
-$db = new Db();
-print_r( $db->query( "SELECT * FROM amministrazioni WHERE `nome`='Milano'" ) );
+class Amministrazioni
+{
+    private $db;
+    private $array;
 
+    public function __construct( $url )
+    {
+        $this->db = new Db();
+        $this->array = $this->db->query( "SELECT * FROM amministrazioni WHERE `url` = '$url'" );
+    }
+
+    public function __destruct()
+    {
+        $this->db->close();
+    }
+
+    public function tAbitanti()
+    {
+        if( $this->array->grado == 3 )
+            return "Il #comune di #" . $this->array->nome . " conta " . $this->array->abitanti . " #abitanti.";
+    }
+}
+
+$url = "http://it.wikipedia.org/wiki/" . "Milano";
+$a = new Amministrazioni( $url );
+echo $a->tAbitanti();
 ?>
