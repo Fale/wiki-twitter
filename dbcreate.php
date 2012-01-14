32a495d2f01bb575b1ec7083cb973a8741c79caa
Fabio Alessandro Locati <flocati@grimp.eu>
2012-01-14 13:31:49 +0100

    Small improvements and checks

--- dbcreate.php
+++ dbcreate.php
@@ -7,11 +7,15 @@ $wiki = new extended;
 $wiki->url = 'http://it.wikipedia.org/w/api.php';
 $db = new Db;
 
-$comuni = $wiki->whatusethetemplate( "Divisione amministrativa" );
+$template = "Divisione amministrativa";
+
+$comuni = $wiki->whatusethetemplate( $template );
+$tpl = $db->query( "SELECT * FROM itwp_templates WHERE `template`='$template'" );
+print_r( $tpl );
 
 foreach( $comuni as $comune )
 {
     $p['url'] = "http://it.wikipedia.org/wiki/" . $comune;
-    $db->smartinsert( $p, "itwp_divamm", "url" );
+    echo $db->smartinsert( $p, "itwp_pages", "url" );
 }
 ?>
