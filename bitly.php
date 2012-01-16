3838d06ab0a147ef84d6cadfa4145fc65e960177
Fabio Alessandro Locati <f.locati@snds.co>
2012-01-16 16:12:34 +0100

    Improve bitly script

--- bitly.php
+++ bitly.php
@@ -5,7 +5,10 @@ require_once( "classes/db.php" );
 $db = new Db;
 $pages = $db->query( "SELECT * FROM itwp_pages WHERE short = ''" );
 foreach( $pages as $page )
+{
     print_r( $page );
-if( $a == 12 )
-    $short_url = Bitly::short('','fabiolocati','R_bf87ec63f9a0a974df98cebebdcb7b8d');
-
+    $r['ID'] = $page['ID'];
+    $r['short'] = Bitly::shorten( $page['url'], 'fabiolocati','R_bf87ec63f9a0a974df98cebebdcb7b8d');
+    $db->update( $r, "itwp_pages", "ID" );
+    print_r( $r );
+}
