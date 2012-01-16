f768b8e0a7b1bc1e2a1a7cdb3933c8ce36414deb
Fabio Alessandro Locati <flocati@grimp.eu>
2012-01-16 16:12:00 +0100

    Use a better bitly implementation

--- classes/bitly.php
+++ classes/bitly.php
@@ -1,12 +1,34 @@
 <?php
 
-class Bitly
-{
-    public static function shorten( $url, $login, $appkey, $format = 'txt' )
-    {
-        $u = 'http://api.bit.ly/v3/shorten?login=' . $login . '&apiKey=' . $appkey . '&uri=' . urlencode( $url ) . '&format=' . $format;
-        return curl_get_result( $u );
+class Bitly {
+
+    private $login;
+    private $appkey;
+
+    function __construct($login, $appkey) {
+        $this->login = $login;
+        $this->appkey = $appkey;
     }
+
+    function shorten($url) {
+        return $this->curl_get_result("http://api.bit.ly/v3/shorten?login={$this->login}&apiKey={$this->appkey}&uri=".urlencode($url)."&format=txt");
+    }
+
+    function expand($url) {
+        return $this->curl_get_result("http://api.bit.ly/v3/expand?login={$this->login}&apiKey={$this->appkey}&shortUrl=".urlencode($url)."&format=txt");
+    }
+
+    private function curl_get_result($url) {
+        $ch = curl_init();
+        $timeout = 5;
+        curl_setopt($ch,CURLOPT_URL,$url);
+        curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+        curl_setopt($ch,CURLOPT_CONNECTTIMEOUT,$timeout);
+        $data = curl_exec($ch);
+        curl_close($ch);
+        return $data;
+    }
+
 }
 
 ?>
