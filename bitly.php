b6e9526c5c212e829c84de693378b2967276b6a2
Fabio Alessandro Locati <f.locati@snds.co>
2012-01-17 12:28:14 +0100

    Now it should not block again

--- bitly.php
+++ bitly.php
@@ -14,5 +14,5 @@ foreach( $pages as $page )
     if( $r['short'] == "INVALID_LOGIN" || $r['short'] == "RATE_LIMIT_EXCEEDED" )
         die();
     $db->update( $r, "itwp_pages", "ID" );
-    usleep( 1000000 );
+    usleep( 3.7 * 1000000 );
 }
