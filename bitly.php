86aba6408e4be6837a5bcc8bbec8ea21edc8abef
Fabio Alessandro Locati <f.locati@snds.co>
2012-01-17 01:04:21 +0100

    Bitly now works

--- bitly.php
+++ bitly.php
@@ -3,12 +3,16 @@ require_once( "classes/bitly.php" );
 require_once( "classes/db.php" );
 
 $db = new Db;
-$pages = $db->query( "SELECT * FROM itwp_pages WHERE short = ''" );
+$bitly = new Bitly( "o_247usnifoe", "R_bf87ec63f9a0a974df98cebebdcb7b8d");
+$pages = $db->query( "SELECT * FROM itwp_pages WHERE short = '' OR short='INVALID_LOGIN' OR short='RATE_LIMIT_EXCEEDED'" );
 foreach( $pages as $page )
 {
     print_r( $page );
     $r['ID'] = $page['ID'];
-    $r['short'] = Bitly::shorten( $page['url'], 'fabiolocati','R_bf87ec63f9a0a974df98cebebdcb7b8d');
-    $db->update( $r, "itwp_pages", "ID" );
+    $r['short'] = $bitly->shorten( $page['url'] );
     print_r( $r );
+    if( $r['short'] == "INVALID_LOGIN" || $r['short'] == "RATE_LIMIT_EXCEEDED" )
+        die();
+    $db->update( $r, "itwp_pages", "ID" );
+    usleep( 700000 );
 }
