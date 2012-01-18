b7f7c404fb425120572ef728d85edea047abb8a3
Fabio Alessandro Locati <f.locati@snds.co>
2012-01-18 09:47:55 +0100

    Improve dbcreate to allow to grep all pages from all controlled templates

--- dbcreate.php
+++ dbcreate.php
@@ -7,19 +7,20 @@ $wiki = new extended;
 $wiki->url = 'http://it.wikipedia.org/w/api.php';
 $db = new Db;
 
-$template = "Divisione amministrativa";
-
-$comuni = $wiki->whatusethetemplate( $template );
-$tpl = $db->query( "SELECT * FROM itwp_templates WHERE `template`='$template'" );
-$tpl = $tpl[0];
-foreach( $comuni as $comune )
+$tpls = $db->query( "SELECT * FROM itwp_templates" );
+foreach( $tpls as $tpl )
 {
-    $p['url'] = "http://it.wikipedia.org/wiki/" . $comune;
-    $r['page'] = $db->smartinsert( $p, "itwp_pages", "url" );
-    if( $r['page'] )
+    $pages = $wiki->whatusethetemplate( $tpl['template'] );
+    foreach( $pages as $page )
     {
-    $r['template'] = $tpl['ID'];
-    $db->insert( $r, "itwp_relations" );
+        $p['url'] = "http://it.wikipedia.org/wiki/" . $page;
+        $r['page'] = $db->smartinsert( $p, "itwp_pages", "url" );
+        print_r( $r );
+        if( $r['page'] )
+        {
+            $r['template'] = $tpl['ID'];
+            $db->insert( $r, "itwp_relations" );
+        }
     }
 }
 ?>
