56f5ec908a5be9436bf60d8c58943629a939de27
Fabio Alessandro Locati <f.locati@snds.co>
2012-02-10 01:53:24 +0100

    Now bio is pretty much done

--- classes/bio.php
+++ classes/bio.php
@@ -7,6 +7,8 @@ class Bio extends TemplateParser
     {
         $a = Array();
         array_push( $a, trim( $this->tBorn( $s ) ) );
+        array_push( $a, trim( $this->tDie( $s ) ) );
+        array_push( $a, trim( $this->tWork( $s ) ) );
         return array_filter( $a );
     }
 
@@ -19,12 +21,42 @@ class Bio extends TemplateParser
             $o .= $this->array['Nome'] . " ";
         if( $this->array['Cognome'] )
             $o .= "#" . $this->array['Cognome'] . " ";
+        return trim( $o );
     }
 
+    private function c( $field, $pre = "", $post = "", $e = "" )
+    {
+        if( $this->array[$field] )
+            return " " . trim( $pre . $this->array[$field] . $post );
+        elseif( $e )
+            return " " . trim( $e );
+    }
+    
     public function tBorn( $s )
     {
-        if( $this->array['AnnoNascita'] && $this->array['GiornoMeseNascita'] )
-            return $this->fullName . " è nato il " . $this->array['GiornoMeseNascita'] . " #" . $this->array['AnnoNascita'] . " a " . $this->array['LuogoNascita'] . ". #sapevatelo $s";
+        if( $this->array['AnnoNascita'] )
+            if( $this->array['Sesso'] == "F" )
+                return $this->fullName() . " è nata" . $this->c( 'GiornoMeseNascita', "il ", "", "nel" ) . " #" . $this->array['AnnoNascita'] . $this->c( 'LuogoNascita', "a #" ) . ". #sapevatelo $s";
+            else
+                return $this->fullName() . " è nato" . $this->c( 'GiornoMeseNascita', "il ", "", "nel" ) . " #" . $this->array['AnnoNascita'] . $this->c( 'LuogoNascita', "a #" ) . ". #sapevatelo $s";
+    }
+
+    public function tDie( $s )
+    {
+        if( $this->array['AnnoMorte'] )
+            if( $this->array['Sesso'] == "F" )
+                return $this->fullName() . " è morta" . $this->c( 'GiornoMeseMorte', "il ", "", "nel" ) . " #" . $this->array['AnnoMorte'] . $this->c( 'LuogoMorte', "a #" ) . ". #sapevatelo $s";
+            else
+                return $this->fullName() . " è morto" . $this->c( 'GiornoMeseMorte', "il ", "", "nel" ) . " #" . $this->array['AnnoMorte'] . $this->c( 'LuogoMorte', "a #" ) . ". #sapevatelo $s";
+    }
+
+    public function tWork( $s )
+    {
+        if( $this->array['Attività'] )
+            if( $this->array['Sesso'] == "F" )
+                return $this->fullName() . " è una #" . $this->array['Attività'] . $this->c( 'Nazionalità', "#" ) . ". #sapevatelo $s";
+            else
+                return $this->fullName() . " è un #" . $this->array['Attività'] . $this->c( 'Nazionalità', "#" ) . ". #sapevatelo $s";
     }
 }
 
