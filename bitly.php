842ce8a3763ef060053966322cb2922805af4215
Fabio Alessandro Locati <f.locati@snds.co>
2012-02-11 15:32:44 +0100

    Improve even more the bitly output

--- bitly.php
+++ bitly.php
@@ -3,11 +3,14 @@ require_once( "classes/bitly.php" );
 require_once( "classes/db.php" );
 
 $db = new Db;
+$item = 0;
 $bitly = new Bitly( "o_247usnifoe", "R_bf87ec63f9a0a974df98cebebdcb7b8d");
 $pages = $db->query( "SELECT * FROM itwp_pages WHERE short = '' OR short='INVALID_LOGIN' OR short='RATE_LIMIT_EXCEEDED'" );
+$items = count( $pages );
 foreach( $pages as $page )
 {
-    echo $page['url'] . " (" . $page['ID'] . ") ";
+    $item = $item + 1;
+    echo "(" . $item . "/" . $items . ") " . $page['url'] . " (" . $page['ID'] . ") ";
     $r['ID'] = $page['ID'];
     $r['short'] = $bitly->shorten( $page['url'] );
     echo "\033[00;32m[ OK ]\033[00m\n";
