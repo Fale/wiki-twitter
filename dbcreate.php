29b5c4753df9e5593dfc590b76a2b9de6a2aaf1f
Fabio Alessandro Locati <flocati@grimp.eu>
2012-02-12 12:57:54 +0100

    Now should be possible to select one template to check

--- dbcreate.php
+++ dbcreate.php
@@ -2,12 +2,19 @@
 
 require_once( "classes/botclasses.php" );
 require_once( "classes/db.php" );
+require_once( "classes/cli.php" );
+
+$args = Cli::parseArgs($_SERVER['argv']);
 
 $wiki = new extended;
 $wiki->url = 'http://it.wikipedia.org/w/api.php';
 $db = new Db;
 
-$tpls = $db->query( "SELECT * FROM itwp_templates" );
+if( $args['tpl'] )
+    $where = " WHERE `template` = " . $args['tpl'] . ";";
+else
+    $where = ";";
+$tpls = $db->query( "SELECT * FROM itwp_templates" . $where );
 foreach( $tpls as $tpl )
 {
     $pages = $wiki->whatusethetemplate( $tpl['template'] );
