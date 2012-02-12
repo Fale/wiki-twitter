8dbd1ac7b3283b5d4397786d0d18b265f6988af5
Fabio Alessandro Locati <flocati@grimp.eu>
2012-02-12 15:49:33 +0100

    Now lock shoud work :)

--- bitly.php
+++ bitly.php
@@ -1,21 +1,26 @@
 <?php
 require_once( "classes/bitly.php" );
 require_once( "classes/db.php" );
+require_once( "classes/cron.php" );
 
-$db = new Db;
-$item = 0;
-$bitly = new Bitly( "o_247usnifoe", "R_bf87ec63f9a0a974df98cebebdcb7b8d");
-$pages = $db->query( "SELECT * FROM itwp_pages WHERE short = '' OR short='INVALID_LOGIN' OR short='RATE_LIMIT_EXCEEDED'" );
-$items = count( $pages );
-foreach( $pages as $page )
+if( ( $pid = Cron::lock() ) !== FALSE )
 {
-    $item = $item + 1;
-    echo "(" . $item . "/" . $items . ") " . $page['url'] . " (" . $page['ID'] . ") ";
-    $r['ID'] = $page['ID'];
-    $r['short'] = $bitly->shorten( $page['url'] );
-    echo "\033[00;32m[ OK ]\033[00m\n";
-    if( $r['short'] == "INVALID_LOGIN" || $r['short'] == "RATE_LIMIT_EXCEEDED" )
-        die();
-    $db->update( $r, "itwp_pages", "ID" );
-    usleep( 4 * 1000000 );
+    $db = new Db;
+    $item = 0;
+    $bitly = new Bitly( "o_247usnifoe", "R_bf87ec63f9a0a974df98cebebdcb7b8d");
+    $pages = $db->query( "SELECT * FROM itwp_pages WHERE short = '' OR short='INVALID_LOGIN' OR short='RATE_LIMIT_EXCEEDED'" );
+    $items = count( $pages );
+    foreach( $pages as $page )
+    {
+        $item = $item + 1;
+        echo "(" . $item . "/" . $items . ") " . $page['url'] . " (" . $page['ID'] . ") ";
+        $r['ID'] = $page['ID'];
+        $r['short'] = $bitly->shorten( $page['url'] );
+        echo "\033[00;32m[ OK ]\033[00m\n";
+        if( $r['short'] == "INVALID_LOGIN" || $r['short'] == "RATE_LIMIT_EXCEEDED" )
+            die();
+        $db->update( $r, "itwp_pages", "ID" );
+        usleep( 4 * 1000000 );
+    }
+    Cron::unlock();
 }
--- bitly.sh
+++
@@ -1,6 +0,0 @@
-if [ -z "$(pgrep bitly)" ]
-    then
-        echo "bitly is not running"
-    else
-        echo "bitly is running"
-fi
