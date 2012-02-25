71f8e9ddb611c06883839a47edfb411f8800b4b1
Fabio Alessandro Locati <f.locati@snds.co>
2012-02-25 18:08:48 +0100

    Add some code to make the --send option functioning

--- tweet.php
+++ tweet.php
@@ -2,11 +2,13 @@
 
 ### SETTINGS ###
 require_once( "settings/app.php" );
+require_once( "settings/db.php" );
 
 ### CLASSES ###
 require_once( "classes/dyrki/dyrki.php" );
 require_once( "classes/twitter/twitter.class.php" );
 require_once( "classes/cli.php" );
+require_once( "classes/db.php" );
 
 $args = Cli::parseArgs($_SERVER['argv']);
 
@@ -14,12 +16,13 @@ if( ! $args['user'] )
     die( "Uso: tweet.php --user=# [--send]\n" );
 
 $dyrki = new Dyrki( $args['user'] );
+$db = new Db();
 $tweet = $dyrki->createTweets();
-
+$tdata = $db->query( "SELECT * FROM accounts WHERE `ID` = " . $args['user'] . ";" );
 if( $args['send'] )
 {
-    $twitter = new Twitter( $twitterKey, $twitterSecret, $token, $secret );
-    $t = $twitter->send( $out[array_rand( $out )] );
+    $twitter = new Twitter( $twitterKey, $twitterSecret, $tdata['0']['token'], $tdata['0']['secret'] );
+    $t = $twitter->send( $tweet[array_rand( $tweet )] );
 }
 else
 {
