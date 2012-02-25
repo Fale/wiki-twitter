2aca4a768085569e3c2b6fdde8124d13bbcec19a
Fabio Alessandro Locati <f.locati@snds.co>
2012-02-25 16:49:06 +0100

    improve dbcreate.php

--- dbcreate.php
+++ dbcreate.php
@@ -4,36 +4,44 @@ require_once( "classes/wiki/botclasses.php" );
 require_once( "classes/db.php" );
 require_once( "classes/cli.php" );
 
-$args = Cli::parseArgs($_SERVER['argv']);
-if( ! $args['source'] )
-        die( "Uso: tweet.php --source=# [--template]\n" );
-
-$db = new Db;
-$src = $db->query( "SELECT * FROM `sources` WHERE `ID_source` = '" . $args['source'] . "';" );
-$prefix = $src['0']['prefix'];
-$apiurl = $src['0']['apiurl'];
-$url = $src['0']['url'];
-$wiki = new extended( $apiurl );
-
-if( $args['template'] )
-    $where = " WHERE `template` = '" . $args['tpl'] . "';";
-else
-    $where = ";";
-
-$tpls = $db->query( "SELECT * FROM " . $prefix . "_templates" . $where );
-foreach( $tpls as $tpl )
+function pages( $args )
 {
-    $pages = $wiki->whatusethetemplate( $tpl['template'] );
-    foreach( $pages as $page )
+    $db = new Db;
+    $src = $db->query( "SELECT * FROM `sources` WHERE `ID_source` = '" . $args['source'] . "';" );
+    $prefix = $src['0']['prefix'];
+    $apiurl = $src['0']['apiurl'];
+    $url = $src['0']['url'];
+    $wiki = new extended( $apiurl );
+
+    if( $args['template'] )
+        $where = " AND `template` = '" . $args['template'] . "';";
+    else
+        $where = ";";
+
+    $tpls = $db->query( "SELECT * FROM templates WHERE `ID_source` = " . $args['source'] . $where );
+    foreach( $tpls as $tpl )
     {
-        $p['url'] = $url . $page;
-        $r['page'] = $db->smartinsert( $p, $prefix . "_pages", "url" );
-        echo $p['url'] . " (Tpl:" . $tpl['template'] . ") (ID:" . $r['page'] . ")\n";
-        if( $r['page'] )
+        $pages = $wiki->whatusethetemplate( $tpl['template'] );
+        foreach( $pages as $page )
         {
-            $r['template'] = $tpl['ID'];
-            $db->insert( $r, $prefix . "_relations" );
+            $p['url'] = $url . $page;
+            $r['page'] = $db->smartinsert( $p, $prefix . "_pages", "url" );
+            echo $p['url'] . " (Tpl:" . $tpl['template'] . ") (ID:" . $r['page'] . ")\n";
+            if( $r['page'] )
+            {
+                $r['template'] = $tpl['ID'];
+                $db->insert( $r, $prefix . "_relations" );
+            }
         }
     }
 }
+
+$args = Cli::parseArgs($_SERVER['argv']);
+if( $args[0] == "pages" )
+    if( !$args['source'] )
+        die( "Uso: dbcreate.php pages --source=# [--template]\n" );
+    else
+        pages( $args );
+else
+    die( "Uso: dbcreate COMMAND [parameters]\nCOMMANDS:\n*pages\tmanage pages" );
 ?>
