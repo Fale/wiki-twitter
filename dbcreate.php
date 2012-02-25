0d93dc7bb76062a26148cdd7e47c5fc169fe60a8
Fabio Alessandro Locati <f.locati@snds.co>
2012-02-25 16:35:04 +0100

    Update dbcreate to last db version

--- dbcreate.php
+++ dbcreate.php
@@ -5,28 +5,34 @@ require_once( "classes/db.php" );
 require_once( "classes/cli.php" );
 
 $args = Cli::parseArgs($_SERVER['argv']);
+if( ! $args['source'] )
+        die( "Uso: tweet.php --source=# [--template]\n" );
 
-$wiki = new extended;
-$wiki->url = 'http://it.wikipedia.org/w/api.php';
 $db = new Db;
+$src = $db->query( "SELECT * FROM `sources` WHERE `ID_source` = '" . $args['source'] . "';" );
+$prefix = $src['0']['prefix'];
+$apiurl = $src['0']['apiurl'];
+$url = $src['0']['url'];
+$wiki = new extended( $apiurl );
 
-if( $args['tpl'] )
+if( $args['template'] )
     $where = " WHERE `template` = '" . $args['tpl'] . "';";
 else
     $where = ";";
-$tpls = $db->query( "SELECT * FROM itwp_templates" . $where );
+
+$tpls = $db->query( "SELECT * FROM " . $prefix . "_templates" . $where );
 foreach( $tpls as $tpl )
 {
     $pages = $wiki->whatusethetemplate( $tpl['template'] );
     foreach( $pages as $page )
     {
-        $p['url'] = "http://it.wikipedia.org/wiki/" . $page;
-        $r['page'] = $db->smartinsert( $p, "itwp_pages", "url" );
+        $p['url'] = $url . $page;
+        $r['page'] = $db->smartinsert( $p, $prefix . "_pages", "url" );
         echo $p['url'] . " (Tpl:" . $tpl['template'] . ") (ID:" . $r['page'] . ")\n";
         if( $r['page'] )
         {
             $r['template'] = $tpl['ID'];
-            $db->insert( $r, "itwp_relations" );
+            $db->insert( $r, $prefix . "_relations" );
         }
     }
 }
