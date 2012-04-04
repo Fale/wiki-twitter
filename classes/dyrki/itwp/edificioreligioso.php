9b7f860bc1b3d01e6b631ae59aad994cd45fb3e9
Fabio Alessandro Locati <f.locati@snds.co>
2012-04-04 18:47:52 +0200

    Initial work on dyrki.itwp.edificioreligioso

---
+++ classes/dyrki/itwp/edificioreligioso.php
@@ -0,0 +1,50 @@
+<?php
+define( '__ROOT__', dirname( dirname( dirname( __FILE__ ) ) ) );
+### CLASSES ###
+require_once( __ROOT__ . "/classes/wiki/templateparser.php" );
+
+class EdificioReligioso extends TemplateParser
+{
+
+    public function tAll( $s )
+    {
+        $a = Array();
+        if( $this->array['Titolo'] )
+        {
+            if( $this->devel['1'] )
+                array_push( $a, trim( $this->tArtista( $s ) ) );
+            if( $this->devel['1'] )
+                array_push( $a, trim( $this->tMateriale( $s ) ) );
+            if( $this->devel['3'] )
+                array_push( $a, trim( $this->tTecnica( $s ) ) );
+            if( $this->devel['1'] )
+                array_push( $a, trim( $this->tAltezza( $s ) ) );
+        }
+        return array_filter( $a );
+    }
+
+    public function tTecnica( $s )
+    {
+        if( $this->array['Tecnica'] )
+            return "\"" . $this->array['Titolo'] . "\" è un dipinto #" . $this->array['Tecnica'] . ". #sapevatelo $s";
+    }
+
+    public function tAltezza( $s )
+    {
+        if( $this->array['Altezza'] )
+            return "\"" . $this->array['Titolo'] . "\" è " . $this->opera() . " alt" . ($this->array['Opera'] == "dipinto" ? "o":"a") . " " . $this->array['Altezza'] . " #cm. #sapevatelo $s";
+    }
+
+    public function tMateriale( $s )
+    {
+        if( $this->array['Materiale'] )
+            return "\"" . $this->array['Titolo'] . "\" è una scultura in #" . $this->array['Materiale'] . ". #sapevatelo $s";
+    }
+
+    public function tArtista( $s )
+    {
+        if( $this->array['Artista'] )
+            return "\"" . $this->array['Titolo'] . "\" è " . $this->opera() . " di #" . $this->array['Artista'] . ". #sapevatelo $s";
+    }
+}
+?>
